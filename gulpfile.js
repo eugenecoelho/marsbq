9597b661cc59afc73cdf10171a1c611a4310fe93
eugenecoelho <eugenecoelho@gmail.com>
2016-04-24 21:10:01 +0100

    set up handlebars templates'

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,55 @@
 var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+var minifyCss = require('gulp-minify-css');
+var sourcemaps = require('gulp-sourcemaps');
+var uglify = require('gulp-uglify');
+var imagemin = require('gulp-imagemin');
+var handlebars = require('gulp-compile-handlebars');
+var rename = require('gulp-rename');
 
-gulp.task('default',function(){
+gulp.task('templates', function(){
+	return gulp.src(['src/templates/**/*.hbs'])
+	.pipe(handlebars())
+	.pipe(rename(function(path){
+		path.extname = '.html'
+	}))
+	.pipe(gulp.dest('./'));
+});
+
+gulp.task('images', function(){
+	gulp.src(['src/img/**/*'])
+	.pipe(imagemin())
+	.pipe(gulp.dest('dist/img'))
+	.pipe(browserSync.stream());
+});
+
+gulp.task('scripts', function(){
+	gulp.src(['src/scripts/main.js'])
+	.pipe(sourcemaps.init())
+	.pipe(uglify())
+	.pipe(sourcemaps.write())
+	.pipe(gulp.dest('dist/scripts'))
+	.pipe(browserSync.stream());
+});
+
+gulp.task('styles', function() {
+ gulp.src(['src/styles/**/*.css'])
+  .pipe(sourcemaps.init())
+  .pipe(minifyCss())
+  .pipe(sourcemaps.write())
+  .pipe(gulp.dest('dist/styles'))
+  .pipe(browserSync.stream());
+});
+
+gulp.task('default',['styles','images','scripts','templates'],function(){
 	console.log('Your first task has run');
+	browserSync.init({
+		server:'./'
+	});
+	// gulp.watch('src/**/*', browserSync.reload);
+	gulp.watch('src/styles/**/*.css', ['styles']);
+	gulp.watch('src/img/**/*', ['images']);
+	gulp.watch('src/scripts/**/*.js', ['scripts']);
+	gulp.watch('src/templates/**/*.hbs', ['templates']);
+	gulp.watch('*.html',browserSync.reload);
 })
